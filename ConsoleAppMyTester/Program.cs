9edb81cbd20abba35b149411ea9f2ff962da79c9
Dima <73338242+Dima3108@users.noreply.github.com>
2025-01-29 17:18:02 +0300

    t

--- ConsoleAppMyTester/Program.cs
+++ ConsoleAppMyTester/Program.cs
@@ -2,6 +2,7 @@ namespace ConsoleAppMyTester;
 
 using System.Text;
 using SharpCompress;
+using SharpCompress.Compressors.LZMA;
 using SharpCompress.Factories;
 using SharpCompress.Writers.Zip;
 internal class Program
@@ -20,6 +21,32 @@ internal class Program
             arc.Write("content.txt", new MemoryStream(UnicodeEncoding.UTF8.GetBytes(content)),DateTime.Now);
             arc.Dispose();
         }
-        
+        var tarfactory=new TarFactory();
+        /*using(Stream s = File.OpenWrite("content.tar.xz"))
+        {
+            using (var arc = tarfactory.Open(s, new SharpCompress.Writers.Tar.TarWriterOptions(SharpCompress.Common.CompressionType.Xz, true)))
+            {
+                arc.Write("content.txt", new MemoryStream(UnicodeEncoding.UTF8.GetBytes(content)), DateTime.Now);
+                arc.Dispose();
+            }
+        }*/
+        using(LzmaStream xz=new LzmaStream(new LzmaEncoderProperties(), false, File.OpenWrite("t.txt.lzma")))
+        {
+            xz.Write(UnicodeEncoding.UTF8.GetBytes(content));
+            xz.Flush();
+            xz.Close();
+        }
+        Console.ReadLine();
+        const string filename = @"Q:/GITHub/LibreOffice.zip";
+        /*using (Stream s = File.OpenRead(filename))
+        {
+            var arc = factory.OpenReader(s, new SharpCompress.Readers.ReaderOptions());
+           
+            while (arc.MoveToNextEntry())
+            {
+                Console.WriteLine(arc.Entry.Key);
+            }
+           
+        }*/
     }
 }
